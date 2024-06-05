f48f2df514fee699fa1a171227faed3d4f470a6b
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-05 11:29:20 -0300

    feat: add varibles to resources configurations

--- variables.tf
+++ variables.tf
@@ -17,3 +17,21 @@ variable "az_client_secret" {
   type        = string
   description = "azure clinet secret"
 }
+
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Local do grupo de recursos"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefixo para o grupo de recursos que vai ser combinado com um nome randomico."
+}
+
+variable "username" {
+  type        = string
+  description = "O usuario que vai ser usado pra acessar a VM."
+  default     = "azureadmin"
+}
